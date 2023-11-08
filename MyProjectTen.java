18d6f9b503198faaf79855f096ea29a7056515c4
Ankit Patel <ankitpatel1984@gmail.com>
2023-11-08 17:15:54 +0000

    MyProjectTen

---
+++ MyProjectTen.java
@@ -0,0 +1,15 @@
+package myprojectten;
+
+public class MyProjectTen {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 30; a++) {
+            if (a % 2 == 0) {
+                System.out.println(a + " is even");
+            } else {
+                System.out.println(a  + " is odd");
+            }
+    }
+
+
+    }
+}
